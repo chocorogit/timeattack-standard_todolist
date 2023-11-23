751f3a5314cdc5957029efb71beedfebb2e7230f
chocorogit <chocorogit@gmail.com>
2023-11-23 12:49:22 +0900

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
+import shortid from "shortid";
+import styled from "styled-components";
 
 const Home = () => {
-  return <div></div>;
+    const initialState = [
+        {
+            id: shortid.generate(),
+            title: "제목1",
+            content: "내용1",
+            isDone: false,
+        },
+        {
+            id: shortid.generate(),
+            title: "제목2",
+            content: "내용2",
+            isDone: true,
+        },
+        {
+            id: shortid.generate(),
+            title: "제목3",
+            content: "내용3",
+            isDone: false,
+        },
+        {
+            id: shortid.generate(),
+            title: "제목4",
+            content: "내용4",
+            isDone: true,
+        },
+    ];
+    const [todos, setTodos] = useState(initialState);
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+
+    const titleHandler = (e) => {
+        setTitle(e.target.value);
+    };
+
+    const contentHandler = (e) => {
+        setContent(e.target.value);
+    };
+
+    const submitTodo = (e) => {
+        e.preventDefault();
+        const newTodo = {
+            id: shortid.generate(),
+            title,
+            content,
+            isDone: false,
+        };
+        setTodos([...todos, newTodo]);
+        setTitle("");
+        setContent("");
+    };
+
+    return (
+        <>
+            <form onSubmit={submitTodo}>
+                <input value={title} onChange={titleHandler} />
+                <input value={content} onChange={contentHandler} />
+                <button type="submit">입력</button>
+            </form>
+            <h2>할 일</h2>
+            {todos
+                .filter((todo) => !todo.isDone)
+                .map((todo) => (
+                    <Todo key={todo.id}>
+                        <span>{todo.id}</span>
+                        <h4>{todo.title}</h4>
+                        <p>{todo.content}</p>
+                        <p>{todo.isDone}</p>
+                        <button>완료</button>
+                        <button>삭제</button>
+                    </Todo>
+                ))}
+            <h2>완료</h2>
+            {todos
+                .filter((todo) => todo.isDone)
+                .map((todo) => (
+                    <Todo key={todo.id}>
+                        <span>{todo.id}</span>
+                        <h4>{todo.title}</h4>
+                        <p>{todo.content}</p>
+                        <p>{todo.isDone}</p>
+                        <button>완료 취소</button>
+                        <button>삭제</button>
+                    </Todo>
+                ))}
+        </>
+    );
 };
+
+const Todo = styled.div`
+    border: 1px solid #999;
+`;
 
 export default Home;
--- src/redux/modules/todos.js
+++ src/redux/modules/todos.js
@@ -1,30 +1,42 @@
 // import uuid from "react-uuid";
+import { useState } from "react";
 import shortid from "shortid";
+import { createStore } from "redux";
+import { useSelector } from "react-redux";
 
 const initialState = [
-  {
-    id: shortid.generate(),
-    title: "",
-    body: "",
-    isDone: false,
-  },
+    {
+        id: shortid.generate(),
+        title: "제목1",
+        content: "내용1",
+        isDone: false,
+    },
+    {
+        id: shortid.generate(),
+        title: "제목2",
+        content: "내용2",
+        isDone: true,
+    },
 ];
 
 // 리듀서
-const todos = (state = initialState, action) => {
-  switch (action.type) {
-    case "ADD_TODO":
-      return; //TODO: 여기 작성
+export const todos = (state = initialState, action) => {
+    switch (action.type) {
+        case "ADD_TODO":
+            return [...state, action.payload];
 
-    case "DELETE_TODO":
-      return; //TODO: 여기 작성
+        case "DELETE_TODO":
+            // TODO: 여기 작성
+            return state;
 
-    case "SWITCH_TODO":
-      return; //TODO: 여기 작성
+        case "SWITCH_TODO":
+            // TODO: 여기 작성
+            return state;
 
-    default:
-      return state;
-  }
+        default:
+            return state;
+    }
 };
 
-export default todos;
+// 스토어 생성
+const store = createStore(todos);
